fd7f21debd89f1915327babca604aaae47e8def5
Dmitry Hramenkov <dmitry.hramenkov@gmail.com>
2026-09-12 22:13:55 +0300

    Переделал задание согласно описанию. Ранее  реализованный класс переименовал в CreditCardFail.java

--- src/homework/lesson6/MainRuner.java
+++ src/homework/lesson6/MainRuner.java
@@ -7,17 +7,36 @@ import java.util.Scanner;
 public class MainRuner {
     static void main() {
 
-        CreditCard creditCard = new CreditCard();
         Scanner scanner = new Scanner(System.in);
+        CreditCard creditCard1 = new CreditCard();
+        CreditCard creditCard2 = new CreditCard();
+        CreditCard creditCard3 = new CreditCard();
+        creditCard1.setNumberCart(1);
+        creditCard1.setMoney(200);
+
+        creditCard2.setNumberCart(2);
+        creditCard2.setMoney(400);
+
+        creditCard3.setNumberCart(3);
+        creditCard3.setMoney(700);
         //пополнение карт деньгами
-       creditCard.toppicUpCards();
-       System.out.println("Введите номер карты (1,2,3) на которую хотите положить деньги и укажите сумму");
-       creditCard.putMoney(scanner.nextInt(), scanner.nextDouble());
-        System.out.println("Введите номер карты (1,2,3) на которую хотите положить деньги и укажите сумму");
-        creditCard.putMoney(scanner.nextInt(), scanner.nextDouble());
-        System.out.println("Введите номер карты (1,2,3) с которой хотите снять деньги и укажите сумму");
-       creditCard.getMoney(scanner.nextInt(), scanner.nextDouble());
-       creditCard.informationAboutCards();
+        creditCard1.setMoney(scanner.nextDouble());
+        creditCard1.methodPutMoney(creditCard1.getNumberCart(), creditCard1.getMoney());
+        creditCard2.methodPutMoney(creditCard2.getNumberCart(), creditCard2.getMoney());
+        creditCard3.methodGetMoney(creditCard3.getNumberCart(), creditCard3.getMoney());
+        creditCard1.informationCard();
+        creditCard2.informationCard();
+        creditCard3.informationCard();
+
+
+//       creditCard.toppicUpCards();
+//       System.out.println("Введите номер карты (1,2,3) на которую хотите положить деньги и укажите сумму");
+//       creditCard.putMoney(scanner.nextInt(), scanner.nextDouble());
+//        System.out.println("Введите номер карты (1,2,3) на которую хотите положить деньги и укажите сумму");
+//        creditCard.putMoney(scanner.nextInt(), scanner.nextDouble());
+//        System.out.println("Введите номер карты (1,2,3) с которой хотите снять деньги и укажите сумму");
+//       creditCard.getMoney(scanner.nextInt(), scanner.nextDouble());
+//       creditCard.informationAboutCards();
 
     }
 }
--- src/homework/lesson6/mainTask/CreditCard.java
+++ src/homework/lesson6/mainTask/CreditCard.java
@@ -1,50 +1,36 @@
 package homework.lesson6.mainTask;
 
-import java.util.Arrays;
-import java.util.Scanner;
-
 public class CreditCard {
-    int [] numberCard = new int[3];
-    double[] money =new double[3];
+    private int numberCart;
+    private double money;
 
-    Scanner scanner = new Scanner(System.in);
-
-    public void toppicUpCards(){
-        for(int index=0; index < numberCard.length; index ++){
-            numberCard[index] = index+1;
-            money[index] = 100;
-        }
+    public int getNumberCart() {
+        return numberCart;
     }
 
-    public void putMoney(int number, double money){
-        if(number != 0 && number <= 3){
-            this.money[number-1] += money;
-        }
-        else {
-            System.out.println("Веденной карты не существует");
-        }
-
+    public double getMoney() {
+        return money;
     }
 
-    public void getMoney(int number, double money){
-       if (money > this.money[number-1]){
-            System.out.println("Введена сумма больше чем есть на карте");
-        }
-        else if(number != 0 && number <= 3){
-            this.money[number-1] -= money;
-        }
-        else {
-            System.out.println("Веденной карты не существует");
-        }
+    public void setNumberCart(int numberCart) {
+        this.numberCart = numberCart;
     }
 
-    public void informationAboutCards() {
-        for (int index =0; index < numberCard.length; index++){
-            System.out.print("Баланс карты " + (index+1) + ": ");
-            System.out.println(this.money[index]);
-        }
+    public void setMoney(double money) {
+        this.money = money;
     }
 
+    public void methodPutMoney(int numberCart, double money){
+       this.numberCart = numberCart;
+        this.money += money;
+    }
 
+    public void methodGetMoney(int numberCart, double money){
+        this.numberCart = numberCart;
+        this.money -= money;
+    }
 
+    public void informationCard(){
+        System.out.println("Баланс карты " + getNumberCart() + " состовляет " + getMoney() + "$");
+    }
 }
